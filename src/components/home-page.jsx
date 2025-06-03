ffead59fd3715894f55c981cf4168a7e0577939a
Peter Joseph Buddendeck <pjbuddendeck@wm.edu>
2025-06-03 17:23:37 -0400

    Added navigation bar and toggleable side menu

--- src/components/home-page.jsx
+++ src/components/home-page.jsx
@@ -1,10 +1,57 @@
-import React from "react";
+import React, {useState, useEffect, useRef} from "react";
+import './style.css';
+import logo from '../images/logo.png'
 
 const HomePage = () => {
+    const [menuOn, setMenu] = useState(false);
+    const menuRef = useRef(null);
+    
+    const toggleMenu = () => {
+        setMenu(!menuOn);
+    }
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+        if (menuRef.current && !menuRef.current.contains(event.target)) {
+            setMenu(false);
+        }
+        }
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+        document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [menuRef]);
 
     return (
         <div>
-            <p> test</p>
+            <div id='navbar'>
+                {menuOn && (<ul ref = {menuRef} className="menu">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/projects">Projects</a></li>
+                    <li><a href="/hobbies">Hobbies</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>)}
+                <div className={menuOn ? "blur" : "unblur"}>
+                    <ul>
+                        <li onClick={toggleMenu}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></li>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/projects">Projects</a></li>
+                        <li><a href="/hobbies">Hobbies</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                    </ul>
+                    <img src={logo} alt="Website logo" />
+                </div>
+            </div>
+            <div id='content' className={menuOn ? "blur" : "unblur"}>
+                <div className='openingimage'>
+
+                </div>
+                <div className='about'>
+
+                </div>
+            </div>
         </div>
     );
 };
