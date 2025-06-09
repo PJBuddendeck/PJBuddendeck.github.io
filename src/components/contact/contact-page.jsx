e63cc6d75aba4e88ca66a3c06c45ca47c5ac4b65
Peter Joseph Buddendeck <pjbuddendeck@wm.edu>
2025-06-09 16:27:13 -0400

    Implemented basic email contact form

--- src/components/contact/contact-page.jsx
+++ src/components/contact/contact-page.jsx
@@ -1,10 +1,14 @@
 import React, {useState, useEffect, useRef} from "react";
 import '../style.css';
+import './contact-style.css';
 import logo from '../../images/logo.png';
 
 const ContactPage = () => {
     const [menuOn, setMenu] = useState(false);
+    const [showSuccess, setSuccess] = useState(false);
     const menuRef = useRef(null);
+    const formRef = useRef(null);
+    const succRef = useRef(null);
     
     const toggleMenu = () => {
         setMenu(!menuOn);
@@ -21,6 +25,32 @@ const ContactPage = () => {
         document.removeEventListener('mousedown', handleClickOutside);
         };
     }, [menuRef]);
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "f7f5e1ee-1cc2-42f5-b6cc-865125a26dde");
+
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+
+        const res = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: json
+        }).then((res) => res.json());
+
+        if (res.success) {
+            console.log("Success", res);
+            setSuccess(true);
+        }
+
+        formRef.current.reset();
+    };
 
     return (
         <div>
@@ -47,7 +77,26 @@ const ContactPage = () => {
             </div>
             <div id="content" className={menuOn ? "blur" : "unblur"}>
                 <div id="backdrop">
-                    <h1>This page is under construction!</h1>
+                    <div id='contact-form'>
+                        {showSuccess && (<div id='form-success' ref={succRef}>
+                            The form has been successfully submitted!
+                        </div>)}
+                    <h1>Contact Form</h1>
+                        <form ref={formRef} onSubmit={onSubmit}>
+                            <div className='input'></div>
+                                <label>Full Name</label><br />
+                                <input id="name" type="text" className='field' placeholder="Please enter your full name." name='name' autoComplete="on" required/>
+                            <div className='input'></div>
+                                <label>Email Address</label><br />
+                                <input id="email" type="email" className='field' placeholder="Please enter your email address." name='email' autoComplete="on" required/>
+                            <div className='input'></div>
+                                <label>Message</label><br />
+                                <textarea name='message' className='message' placeholder="Please enter your message." required></textarea>
+                            <div className='buttonarea'>
+                                <br /><button type="submit">Send Message</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
