e7ec29648e7724a299149f6146f6e221fc432f53
Peter Joseph Buddendeck <pjbuddendeck@wm.edu>
2025-07-01 13:56:17 -0400

    Added colors based on project language

--- src/components/projects/items/table-item.jsx
+++ src/components/projects/items/table-item.jsx
@@ -3,12 +3,23 @@ import './item-style.css';
 import personal from '../../../images/personal-project.png';
 import academic from '../../../images/academic-project.png';
 
+const languageColors = {
+    React: '#61dafb',
+    HTML: '#e34c26',
+    JavaScript: '#f7df1e',
+    Python: '#3572A5',
+    C: '#555555',
+    'C++': '#f34b7d',
+    Java: '#b07219',
+};
+
 const TableItem = ({ id, name, desc, language, time, type, onClick }) => {
     const typeToImg = {
         Personal: personal,
         Academic: academic
     };
     const imgSrc = typeToImg[type] || personal;
+    const iconColor = languageColors[language] || '#999999';
 
     return (
         <div className="card" onClick={onClick} style={{ cursor: 'pointer' }}>
@@ -16,7 +27,7 @@ const TableItem = ({ id, name, desc, language, time, type, onClick }) => {
             <img src={imgSrc} alt="Project Thumbnail" className="project-thumbnail" />
             <p className="project-desc">{desc}</p>
             <div className="project-footer">
-                <span className="project-language"><div id="icon"></div>{language}</span>
+                <span className="project-language"><div id="icon" style={{ backgroundColor: iconColor }}></div>{language}</span>
                 <span className="project-time">{time}</span>
                 <span className="project-type">{type}</span>
             </div>
